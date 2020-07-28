99a27bd052ab86014108c56808adc067ee153be5
RyoIto0911 <night224@icloud.com>
2020-07-28 15:36:05 +0900

    挙動確認修正２

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -11,7 +11,6 @@ $(function(){
               ${message.created_at}
             </div>
           </div>
-          <div class="message-box">
             <p class="message-log">
               ${message.content}
             </p>
